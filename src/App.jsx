ecdc16b0ee3782e0764af1abd256d3d503461d73
ruwindya <dilshanwimalasiri94@gmail.com>
2024-02-24 01:17:40 +0530

    update the header and make the profile page private

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Signup from './pages/Signup'
 import Profile from './pages/Profile'
 import Home from './pages/Home'
 import Header from './components/Header'
+import PrivateRoute from './components/PrivateRoute'
 
 function App() {
   return (
@@ -16,7 +17,9 @@ function App() {
           <Route path="/about" element={<About />}/>
           <Route path="/sign-in" element={<Signin />}/>
           <Route path="/sign-up" element={<Signup />}/>
-          <Route path="/profile" element={<Profile />}/>
+          <Route  element={<PrivateRoute />}>
+            <Route path="/profile" element={<Profile />}/>
+          </Route>
         </Routes>
     
     </BrowserRouter>
--- src/components/OAuth.jsx
+++ src/components/OAuth.jsx
@@ -20,7 +20,7 @@ function OAuth() {
                 headers:{
                   'Content-Type':'application/json',
                 },
-                body:JSON.stringify({name : result.user.displayName, email: result.user.email, photo: result.user.photoURL}),
+                body:JSON.stringify({name : result.user.displayName, email: result.user.email, photo: result.user.photoURL,}),
             })
 
             const data = await res.json();
