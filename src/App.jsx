fe7a2227d5f64e439aae4368a52a12c48cd6706d
ruwindya <dilshanwimalasiri94@gmail.com>
2024-02-26 01:14:12 +0530

    complete listing page

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import Header from './components/Header'
 import PrivateRoute from './components/PrivateRoute'
 import CreateListing from './pages/CreateListing'
 import UpdateListing from './pages/UpdateListing'
+import Listing from './pages/Listing'
 
 function App() {
   return (
@@ -19,6 +20,7 @@ function App() {
           <Route path="/about" element={<About />}/>
           <Route path="/sign-in" element={<Signin />}/>
           <Route path="/sign-up" element={<Signup />}/>
+          <Route path="/listing/:listingId" element={<Listing />}/>
           <Route  element={<PrivateRoute />}>
             <Route path="/profile" element={<Profile />}/>
             <Route path="/create-listing" element={<CreateListing />}/>
